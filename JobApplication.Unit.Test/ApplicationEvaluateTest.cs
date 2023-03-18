2e2b248179c343cfaf6c3ccffaa25cd1a16e09d0
UmidShirinov <sirinovumid623@gmail.com>
2023-03-18 13:57:48 +0100

    example teting succesdful

--- JobApplication.Unit.Test/ApplicationEvaluateTest.cs
+++ JobApplication.Unit.Test/ApplicationEvaluateTest.cs
@@ -1,3 +1,6 @@
+using JobApplication.Models;
+using JobApplication.Service;
+
 namespace JobApplication.Unit.Test
 {
     public class ApplicationEvaluateTest
@@ -5,8 +8,54 @@ namespace JobApplication.Unit.Test
 
 
         [Test]
-        public void AAA()
+        public void Application_WithUnderAge_TransferrredToAutoRejected()
         {
+            //Arrange
+            var evaluater = new ApplicationEvaluatot();
+            var form = new JobApply()
+            {
+                Applicant= new Applicant() {
+                    Age = 17
+
+                },
+
+                
+            };
+
+            //action
+            var appResult = evaluater.Evalute(form);
+
+            //Assert
+            Assert.AreEqual(appResult, ResultEnum.ApplicationResult.AutoRejected);
+
+
+
+
+
+        }
+
+        [Test]
+        public void Application_WitListStack_TransferrredToAutoAccepted()
+        {
+            //Arrange
+            var evaluater = new ApplicationEvaluatot();
+            var form = new JobApply()
+            {
+                Applicant = new Applicant() { Age= 19 },
+                TechStackList = new List<string>() { "C#", "RabbitMQ", "MicroSevice", "OOP" },
+                YearsOfExperience = 7
+
+
+            };
+
+            //action
+            var appResult = evaluater.Evalute(form);
+
+            //Assert
+            Assert.AreEqual(appResult, ResultEnum.ApplicationResult.AutoAccepted);
+
+
+
 
 
         }
--- JobApplication/Service/ApplicationEvaluatot.cs
+++ JobApplication/Service/ApplicationEvaluatot.cs
@@ -1,4 +1,5 @@
 ﻿using JobApplication.Models;
+using System.Linq;
 using static JobApplication.Models.ResultEnum;
 
 namespace JobApplication.Service
@@ -7,13 +8,32 @@ namespace JobApplication.Service
     {
 
         private int minAge = 18;
+        private int autoAcceptedYearOfExperience=6;
+        private List<string> techStackList = new() { "C#", "RabbitMQ", "MicroSevice", "OOP", "WEB API" };
         public ApplicationResult Evalute(JobApply form)
         {
             if (form.Applicant.Age<minAge)
             {
                 return ApplicationResult.AutoRejected;
             }
+            var sr = GetStackSimilarityRate(form.TechStackList);
+            if (sr<25)
+                return ApplicationResult.AutoRejected;
+            if (sr > 75 && autoAcceptedYearOfExperience>form.YearsOfExperience)
+                return ApplicationResult.AutoAccepted;
+
+            
             return ApplicationResult.AutoAccepted;
+        }
+        private double GetStackSimilarityRate(List<string> techStacks)
+        {
+            var matchCount =
+                techStacks
+                    .Where(i => techStackList.Contains(i, StringComparer.OrdinalIgnoreCase))
+                    .Count();
+            var  matchPercent =(double)  (matchCount*100/techStackList.Count);
+
+            return matchPercent;
         }
     }
 }
