f928a51aa1358810b7263ec2cba48aba20b072ec
UmidShirinov <sirinovumid623@gmail.com>
2023-04-15 01:46:44 +0200

    mocking objects and IT keyword using

--- JobApplication.Unit.Test/ApplicationEvaluateTest.cs
+++ JobApplication.Unit.Test/ApplicationEvaluateTest.cs
@@ -42,10 +42,13 @@ namespace JobApplication.Unit.Test
             //Arrange
 
             var mockValidator = new Mock<IIdentityValidator>();
+            mockValidator.Setup(i => i.IsValid(It.IsAny<string>())).Returns(true);  // it key wordu mockingded isdifade  olunurolunur.
+
             var evaluater = new ApplicationEvaluatot(mockValidator.Object);
+            
             var form = new JobApply()
             {
-                Applicant = new Applicant() { Age= 19 },
+                Applicant = new Applicant() { Age= 19 ,IdentityNumber="" },
                 TechStackList = new List<string>() { "C#", "RabbitMQ", "MicroSevice", "OOP" },
                 YearsOfExperience = 7
 
@@ -70,7 +73,9 @@ namespace JobApplication.Unit.Test
         public void Application_WitListStack_TransferrredToAutoRejected()
         {
             //Arrange
-            var evaluater = new ApplicationEvaluatot(null);
+            var mockValidator = new Mock<IIdentityValidator>();
+            mockValidator.Setup(i => i.IsValid(It.IsAny<string>())).Returns(true);
+            var evaluater = new ApplicationEvaluatot(mockValidator.Object);
             var form = new JobApply()
             {
                 Applicant = new Applicant() { Age = 19 },
@@ -85,6 +90,32 @@ namespace JobApplication.Unit.Test
 
             //Assert
             Assert.AreEqual(appResult, ResultEnum.ApplicationResult.AutoRejected);
+
+
+
+
+
+        }
+
+
+        [Test]
+        public void Application_WithValidIDentityNUmber_TransferrredToHR()
+        {
+            //Arrange
+            var mockValidator = new Mock<IIdentityValidator>();
+            mockValidator.Setup(i => i.IsValid(It.IsAny<string>())).Returns(false);
+            var evaluater = new ApplicationEvaluatot(mockValidator.Object);
+            var form = new JobApply()
+            {
+                Applicant = new Applicant() { Age = 19 },
+
+            };
+
+            //action
+            var appResult = evaluater.Evalute(form);
+
+            //Assert
+            Assert.AreEqual(appResult, ResultEnum.ApplicationResult.TransferrredToHR);
 
 
 
